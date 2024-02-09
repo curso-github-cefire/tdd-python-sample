209623838b37bdcef4cdc9ce647b6b4e4a33296e
alexlopezprofe2 <158495473+alexlopezprofe2@users.noreply.github.com>
2024-02-09 21:38:50 +0100

    py

--- ejercicios/operaciones.py
+++ ejercicios/operaciones.py
@@ -1,8 +1,3 @@
-# En principio el alumno solo recibiría la definición de la función en el archivo correspondiente
-def suma(x,y):
 
-    # El alumno deberá implementar correctamente el código de la función para pasar el test correspondiente
-    return x + y
-    
-def numero_par(numero)
+def numero_par(numero):
     return numero % 2 == 0 
--- tests/test_numero_par.py
+++ tests/test_numero_par.py
@@ -7,10 +7,8 @@ from ejercicios.operaciones import numero_par
 # Clase para crear tests. Las funciones de testeo deberán crearse en esta clase
 class TestClass:
 
-    # Test para la operación suma
-    def test_numero_par(self):
-        assert es_par(4), "4 es par"
-        assert es_par(0), "0 es par"
-        assert es_par(-2), "-2, los negativos no son pares"
-        assert not es_par(3), "3 no es par"
-        assert not es_par(-1), "-1 no debería ser par"
+    # Test para comprobar si un numero es par
+    def test_numeropar(self):
+        assert numero_par(4)
+        assert numero_par(0) 
+        assert numero_par(10) 
